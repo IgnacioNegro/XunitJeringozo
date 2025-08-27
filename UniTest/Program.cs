56fc6732c7c08cb75e9d12287b1356bc354f5108
IgnacioNegro <negroignacio2014@gmail.com>
2025-08-27 17:29:06 -0300

    Aplicar .gitignore correctamente

--- UniTest/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-using UniTest;
-
-Console.WriteLine("Hello, World!");
-
-Jeringozo.Encriptar("hola mundo");
-Jeringozo.Desencriptar("hopolapa mupundopo");
