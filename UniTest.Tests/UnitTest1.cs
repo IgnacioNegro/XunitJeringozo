d835bd438d2af6268525ef23702c05efabb14939
IgnacioNegro <negroignacio2014@gmail.com>
2025-08-27 17:32:41 -0300

    Primer commit: proyecto Jeringozo con tests xUnit

---
+++ UniTest.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace UniTest.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ UniTest/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using UniTest;
+
+Console.WriteLine("Hello, World!");
+
+Jeringozo.Encriptar("hola mundo");
+Jeringozo.Desencriptar("hopolapa mupundopo");
